fef08092ea30614fcc28a87ef5a9fe7d796e902a
Valeriu-Andrei Florescu <49231993+valeriuflorescu16@users.noreply.github.com>
2024-08-23 11:20:32 +0100

    Update middleware unit tests to test the post router using supertest

--- tests/middleware/middleware.test.ts
+++ tests/middleware/middleware.test.ts
@@ -1,16 +1,38 @@
 import { Middleware } from '../../src/middleware/middleware';
+import request from 'supertest';
+import express from 'express';
 
 describe("middleware", () => {
     let middleware: Middleware;
+    let message: string;
 
     beforeAll(() => {
         middleware = new Middleware();
+        message = "MSG|^~\\&|SenderSystem|Location|ReceiverSystem|Location|20230502112233\n||DATA^TYPE|123456|P|2.5\nEVT|TYPE|20230502112233\nPRS|1|9876543210^^^Location^ID||Florescu^Valeriu|||M|19990530|\nDET|1|I|^^MainDepartment^101^Room 1|Common Cold"
     })
 
     describe("createRouter", () => {
+        let router: express.Router;
+        let app: express.Application;
+        beforeAll(() => {
+            router = middleware.createRouter();
+            app = express();
+            app.use(router);
+        })
+
         it('creates router', () => {
-            const router = middleware.createRouter();
             expect(router).toBeDefined();
         })
+
+        it('responds with status 200 to /patientMessage post request if valid request body', async () => {
+            const response = await request(app).post('/patientMessage').send({ message });
+            expect(response.status).toBe(200);
+        });
+
+        it('responds with status 400 to /patientMessage post request if invalid request body', async () => {
+            const invalidMessage = 'invalidMessage';
+            const response = await request(app).post('/patientMessage').send({ message: invalidMessage });
+            expect(response.status).toBe(400);
+        });
     })
 })
